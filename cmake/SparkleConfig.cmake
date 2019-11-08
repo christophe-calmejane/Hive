ce4f3eeec6af1ed3d83f205ff0d73274adb33ad8
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2019-11-08 11:30:00 +0100

    Fixed Sparkle bundle on macOS

--- cmake/SparkleConfig.cmake
+++ cmake/SparkleConfig.cmake
@@ -22,10 +22,9 @@ elseif(APPLE)
 	set(SPARKLE_BASE_DIR "${PROJECT_ROOT_DIR}/3rdparty/sparkle")
 	set_source_files_properties(${SPARKLE_BASE_DIR}/Sparkle.framework PROPERTIES MACOSX_PACKAGE_LOCATION Frameworks) # MACOSX_PACKAGE_LOCATION = Place a 'source file' (here it is Sparkle.framework) into the app bundle
 
-	add_library(sparkle SHARED IMPORTED GLOBAL)
+	add_library(sparkle INTERFACE IMPORTED GLOBAL)
 
 	set_target_properties(sparkle PROPERTIES
-		IMPORTED_LOCATION "${SPARKLE_BASE_DIR}/Sparkle.framework/Sparkle"
 		INTERFACE_SOURCES "${SPARKLE_BASE_DIR}/Sparkle.framework" # So that our framework is considered a source file, and copied to the app bundle through MACOSX_PACKAGE_LOCATION
 		INTERFACE_LINK_LIBRARIES "${SPARKLE_BASE_DIR}/Sparkle.framework" # So the framework is linked to the target, as well as adding include search path (automatically done by cmake when detecting a framework)
 	)
