480fdc3b2a4e397ab61e082f887725183b3f7a16
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2021-08-15 16:17:25 +0200

    Added process helper class

---
+++ src/processHelper/processHelper_win32.cpp
@@ -0,0 +1,39 @@
+/*
+* Copyright (C) 2017-2023, Emilien Vallot, Christophe Calmejane and other contributors
+
+* This file is part of Hive.
+
+* Hive is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Hive is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public License
+* along with Hive.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "processHelper.hpp"
+
+#include <Windows.h>
+
+namespace utils
+{
+ProcessHelper::ProcessID ProcessHelper::getCurrentProcessID() noexcept
+{
+	return static_cast<ProcessHelper::ProcessID>(GetCurrentProcessId());
+}
+
+bool ProcessHelper::isProcessRunning(ProcessID const pid) noexcept
+{
+	auto const process = OpenProcess(SYNCHRONIZE, FALSE, pid);
+	auto const ret = WaitForSingleObject(process, 0);
+	CloseHandle(process);
+	return ret == WAIT_TIMEOUT;
+}
+
+} // namespace utils
