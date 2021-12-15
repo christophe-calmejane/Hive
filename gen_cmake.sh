2fef8a1403e1a9b069c1559b24694a19d00d1b99
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2021-12-15 09:41:03 +0100

    Improved Qt detection code with Qt6 support

--- gen_cmake.sh
+++ gen_cmake.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # Useful script to generate project files using cmake
 
-# Qt Version to use
-qtVersion="5.15.2"
-
 # Override default cmake options
 cmake_opt="-DBUILD_HIVE_TESTS=TRUE -DENABLE_HIVE_CPACK=FALSE -DENABLE_CODE_SIGNING=FALSE -DENABLE_HIVE_FEATURE_SPARKLE=FALSE"
+
+# Default values
+default_qt_version="5.15.2"
+default_win_basePath="C:/Qt"
+default_win_arch="msvc2019"
+default_mac_basePath="/Applications/Qt"
+default_mac_arch="clang_64"
+default_linux_basePath="/usr/lib"
+default_linux_arch="$(g++ -dumpmachine)"
 
 ############################ DO NOT MODIFY AFTER THAT LINE #############
 
@@ -37,31 +43,85 @@ if [[ "x${params["use_sparkle"]}" == "xtrue" && $OSTYPE == darwin* ]]; then
 	cmake_opt="${cmake_opt} -DHIVE_DSA_PUB_KEY=${dsaPubKey} -DENABLE_HIVE_FEATURE_SPARKLE=TRUE"
 fi
 
-useSources=0
-overrideQt5dir=0
-Qt5dir=""
+if isLinux; then
+	which g++ &> /dev/null
+	if [ $? -ne 0 ];
+	then
+		echo "ERROR: g++ not found"
+		exit 4
+	fi
+fi
+
+# Parse variables
+overrideQtDir=0
+QtDir=""
+overrideQtVers=0
+QtVersion="${default_qt_version}"
+QtMajorVersion="${default_qt_version%%.*}"
+QtConfigFileName="Qt${QtMajorVersion}Config.cmake"
+
+function build_qt_config_folder()
+{
+	local _retval="$1"
+	local basePath="$2"
+	local arch="$3"
+	local majorVers="$4"
+	local result=""
+
+	if isWindows; then
+		result="${basePath}/${arch}/lib/cmake"
+	elif isMac; then
+		result="${basePath}/${arch}/lib/cmake"
+	elif isLinux; then
+		which g++ &> /dev/null
+		if [ $? -ne 0 ];
+		then
+			echo "ERROR: g++ not found"
+			exit 4
+		fi
+		result="${basePath}/${arch}/cmake"
+	fi
+
+	eval $_retval="'${result}'"
+}
 
 function extend_gc_fnc_help()
 {
-	echo " -source -> Use Qt source instead of precompiled binarie (Default: Not using sources). Do not use with -qt5dir option."
-	echo " -qt5dir <Qt5 CMake Folder> -> Override automatic Qt5_DIR detection with the full path to the folder containing Qt5Config.cmake file (either binary or source)"
+	local default_path=""
+	if isWindows; then
+		build_qt_config_folder default_path "${default_win_basePath}/${default_qt_version}" "${default_win_arch}" "${QtMajorVersion}"
+	elif isMac; then
+		build_qt_config_folder default_path "${default_mac_basePath}/${default_qt_version}" "${default_mac_arch}" "${QtMajorVersion}"
+	elif isLinux; then
+		build_qt_config_folder default_path "${default_linux_basePath}" "${default_linux_arch}" "${QtMajorVersion}"
+	fi
+
+	echo " -qtvers <Qt Version> -> Override the default Qt version (v${default_qt_version}) with the specified one."
+	echo " -qtdir <Qt CMake Folder> -> Override default Qt path (${default_path}) with the specified one."
+	echo "Note: You can also use the -qtdir option to use Qt source instead of precompiled binaries (specify 'Src/qtbase' instead of binary arch)."
 }
 
 function extend_gc_fnc_unhandled_arg()
 {
 	case "$1" in
-		-source)
-			useSources=1
-			return 1
-			;;
-		-qt5dir)
+		-qtvers)
 			shift
 			if [ $# -lt 1 ]; then
-				echo "ERROR: Missing parameter for -qt5dir option, see help (-h)"
+				echo "ERROR: Missing parameter for -qtvers option, see help (-h)"
 				exit 4
 			fi
-			overrideQt5dir=1
-			Qt5dir="$1"
+			overrideQtVers=1
+			QtVersion="$1"
+			return 2
+			;;
+		-qtdir)
+			shift
+			if [ $# -lt 1 ]; then
+				echo "ERROR: Missing parameter for -qtdir option, see help (-h)"
+				exit 4
+			fi
+			overrideQtDir=1
+			QtDir="$1"
 			return 2
 			;;
 	esac
@@ -70,74 +130,72 @@ function extend_gc_fnc_unhandled_arg()
 
 function extend_gc_fnc_postparse()
 {
-	if [[ $useSources -eq 1 && $overrideQt5dir -eq 1 ]]; then
-		echo "ERROR: Cannot use -source and -qt5dir options at the same time."
-		echo "If you want to use a custom source folder, just set -qt5dir to <Qt Source Root Folder>/qtbase/lib/cmake/Qt5"
+	# -qtvers is required if -qtdir is used
+	if [[ ${overrideQtDir} -eq 1 && ${overrideQtVers} -eq 0 ]]; then
+		echo "ERROR: -qtdir option requires -qtvers option, see help (-h)"
 		exit 4
 	fi
+	# Reset QtMajorVersion and QtConfigFileName, since QtVersion might have been overridden by -qtvers option
+	QtMajorVersion="${QtVersion%%.*}"
+	QtConfigFileName="Qt${QtMajorVersion}Config.cmake"
 }
 
 function extend_gc_fnc_precmake()
 {
-	# Using automatic Qt5_DIR detection
-	if [ $overrideQt5dir -eq 0 ]; then
+	# Using automatic Qt detection
+	if [ $overrideQtDir -eq 0 ]; then
 		if isWindows; then
-			qtBasePath="c:/Qt/${qtVersion}"
+			qtBasePath="${default_win_basePath}/${QtVersion}"
 			if [ "$arch" == "x64" ]; then
 				qtArch="msvc2017_64"
 			else
-				qtArch="msvc2019"
+				qtArch="${default_win_arch}"
 			fi
 		elif isMac; then
-			qtBasePath="/Applications/Qt/${qtVersion}"
-			qtArch="clang_64"
+			qtBasePath="${default_mac_basePath}/${QtVersion}"
+			if [ "${QtMajorVersion}" == "6" ] ; then
+				qtArch="macos"
+			else
+				qtArch="${default_mac_arch}"
+			fi
 		elif isLinux; then
 			if [ "x${QT_BASE_PATH}" != "x" ]; then
 				if [ ! -f "${QT_BASE_PATH}/MaintenanceTool" ]; then
 					echo "Invalid QT_BASE_PATH: MaintenanceTool not found in specified folder: ${QT_BASE_PATH}"
-					echo "Maybe try the -qt5dir option, see help (-h)"
+					echo "Maybe try the -qtdir option, see help (-h)"
 					exit 1
 				fi
 
-				qtBasePath="${QT_BASE_PATH}/${qtVersion}"
-				qtArch="gcc_64"
+				qtBasePath="${QT_BASE_PATH}/${QtVersion}"
+				qtArch="" # Maybe use default_linux_arch as well? (if yes, factorize qtArch for both QT_BASE_PATH and system wide)
 			else
-				echo "Using cmake's auto-detection of Qt headers and libraries"
-				echo "QT_BASE_PATH env variable can be defined to the root folder of Qt installation (where MaintenanceTool resides), or the -qt5dir option. See help (-h) for more details."
+				echo "Using system wide Qt headers and libraries."
+				echo "QT_BASE_PATH env variable can be defined to the root folder of Qt installation (where MaintenanceTool resides), or the -qtdir option. See help (-h) for more details."
+				qtBasePath="${default_linux_basePath}"
+				qtArch="${default_linux_arch}"
 			fi
 		else
 			echo "Unsupported platform"
 			exit 1
 		fi
 
-		# Check specified Qt version is available
-		if [ ! -d "${qtBasePath}" ];
-		then
-			echo "Cannot find Qt v$qtVersion installation path."
-			exit 1
-		fi
-
-		# Check specified Qt arch is available
-		if [ ! -d "${qtBasePath}/${qtArch}" ]; then
-			echo "Cannot find Qt arch '${qtArch}' for Qt v${qtVersion}"
-			exit 1
-		fi
-
-		if [ $useSources -eq 1 ]; then
-			# Override qtArch path with Source path
-			qtArch="Src/qtbase"
-			echo "Using Qt source instead of precompiled libraries"
-		fi
-		Qt5dir="${qtBasePath}/${qtArch}/lib/cmake/Qt5"
+		build_qt_config_folder QtDir "${qtBasePath}" "${qtArch}" "${QtMajorVersion}"
 	fi
 
-	# Validate Qt5dir
-	if [ ! -f "${Qt5dir}/Qt5Config.cmake" ]; then
-		echo "Invalid Qt5_DIR folder (${Qt5dir}): Qt5Config.cmake not found in the folder"
+	# Validate QtDir
+	if [ ! -f "${QtDir}/Qt${QtMajorVersion}/${QtConfigFileName}" ]; then
+		echo "Invalid Qt folder (${QtDir}): ${QtConfigFileName} not found in the folder."
+		echo "Maybe try the -qtdir option, see help (-h)"
 		exit 1
 	fi
 
-	add_cmake_opt+=("-DQt5_DIR=${Qt5dir}")
+	add_cmake_opt+=("-DCMAKE_PREFIX_PATH=${QtDir}")
+	add_cmake_opt+=("-DQT_MAJOR_VERSION=${QtMajorVersion}")
+}
+
+function extend_gc_fnc_props_summary()
+{
+	echo "| - QT VERS: ${QtVersion}"
 }
 
 # execute gen_cmake script from bashUtils
