8a1722b71ee8157f46fdb99dd4567c86c2858071
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2022-08-03 16:00:10 +0200

    Hive AEM displays computer name in the GROUP field

---
+++ libs/modelsLibrary/helper_unix.cpp
@@ -0,0 +1,41 @@
+/*
+* Copyright (C) 2017-2022, Emilien Vallot, Christophe Calmejane and other contributors
+
+* This file is part of Hive.
+
+* Hive is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Hive is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+
+* You should have received a copy of the GNU Lesser General Public License
+* along with Hive.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "hive/modelsLibrary/helper.hpp"
+
+#include <unistd.h>
+#include <limits.h>
+
+namespace hive
+{
+namespace modelsLibrary
+{
+namespace helper
+{
+QString getComputerName() noexcept
+{
+	char hostname[HOST_NAME_MAX];
+	gethostname(hostname, HOST_NAME_MAX);
+	return QString{ hostname };
+}
+
+} // namespace helper
+} // namespace modelsLibrary
+} // namespace hive
