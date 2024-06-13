86ccaf0070e6c5149314c256f8bb5fca0f145abd
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2024-06-13 10:07:09 +0200

    Updated gcc warning flags

--- local_definitions.cmake
+++ local_definitions.cmake
@@ -1,5 +1,5 @@
 # Override the default warning flags.
 cu_set_warning_flags(TARGETS QtMate_static COMPILER CLANG PRIVATE -Wno-unused-parameter -Wno-float-conversion) # -Wno-float-conversion to be removed!!
 cu_set_warning_flags(TARGETS Hive_static Hive COMPILER CLANG PRIVATE -Wno-unused-parameter)
-# cu_set_warning_flags(TARGETS la_avdecc_cxx la_avdecc_static la_avdecc_controller_cxx la_avdecc_controller_static COMPILER CLANG PRIVATE -Wno-unused-parameter PUBLIC -Wno-gnu-zero-variadic-macro-arguments)
-# cu_set_warning_flags(TARGETS la_avdecc_cxx la_avdecc_static la_avdecc_controller_cxx la_avdecc_controller_static Tests COMPILER GCC PRIVATE -Wno-ignored-qualifiers)
+cu_set_warning_flags(TARGETS QtMate_static COMPILER GCC PRIVATE -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-float-conversion) # -Wno-float-conversion to be removed!!
+cu_set_warning_flags(TARGETS Hive_widget_models_static Hive_static COMPILER GCC PRIVATE -Wno-float-conversion) # -Wno-float-conversion to be removed!!
